259f7a648533fb757fa2da4807ae6516a1d2b84f
Rainiugnas <pguego@gmail.com>
2015-09-27 15:52:27 +0200

    set keyboard layout

--- install.sh
+++ install.sh
@@ -3,10 +3,19 @@ apt-get update
 apt-get upgrade
 update-manager -c -d
 
-#language
+
+#download language
 apt-get install language-pack-fr-base
 apt-get install language-pack-fr
 apt-get install language-pack-gnome-fr-base
 apt-get install language-pack-gnome-fr
 
+#set language
 update-locale LANG=fr_FR.utf8
+
+#set keyboard layout
+file="/etc/default/keyboard"
+varname="XKBLAYOUT="
+val="fr"
+line=`grep $varname $file`
+sed -i "s/$line/$varname\"$val\"/g" $file
