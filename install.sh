05b39e6e419550ef2dfb9cfdc1e11256291346bf
Rainiugnas <pguego@gmail.com>
2015-09-27 13:46:36 +0200

    download language

--- install.sh
+++ install.sh
@@ -2,3 +2,10 @@
 apt-get update
 apt-get upgrade
 update-manager -c -d
+
+#language
+apt-get install language-pack-fr-base
+apt-get install language-pack-fr
+apt-get install language-pack-gnome-fr-base
+apt-get install language-pack-gnome-fr
+apt-get install language-pack-kde-fr
