7c9be2f14c4f68a252e412e5ae2aaeb8d8145117
Rainiugnas <pguego@gmail.com>
2015-09-27 19:51:28 +0200

    add chrome install

--- install.sh
+++ install.sh
@@ -20,6 +20,14 @@ val="fr"
 line=`grep $varname $file`
 sed -i "s/$line/$varname\"$val\"/g" $file
 
+
 #create file to add bash conf
 touch ~/my_bashrc.sh
 echo -e "\n#Add by ScryptFull\nbash ~/my_bashrc.sh" >> ~/.bashrc
+
+
+#install google chrome
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
+sh -c 'echo "deb https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+apt-get update
+apt-get install google-chrome-stable
