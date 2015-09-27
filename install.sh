e6fd662554823fee35e48645f5352d61d6bd62ee
Rainiugnas <pguego@gmail.com>
2015-09-27 14:19:55 +0200

    config /etc/defautl/locale

--- install.sh
+++ install.sh
@@ -8,3 +8,6 @@ apt-get install language-pack-fr-base
 apt-get install language-pack-fr
 apt-get install language-pack-gnome-fr-base
 apt-get install language-pack-gnome-fr
+
+echo 'LANG="fr_FR.UTF-8"' > /etc/default/locale
+echo 'LANGUAGE="fr_FR:en"' >> /etc/default/locale
