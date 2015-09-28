6c44632c781dc6cfb30c6194f3c6e36ea9c063a3
Rainiugnas <pguego@gmail.com>
2015-09-28 19:53:06 +0200

    Add link to download sublime config

--- install.sh
+++ install.sh
@@ -71,5 +71,5 @@ apt-get update
 apt-get install sublime-text
 
 #sublime's conf
-wget -O ~/.config/sublime-text-2/Packages/User/Preferences.sublime-settings google.fr
-wget -O ~/.config/sublime-text-2/Packages/User/Default\ \(Linux\).sublime-keymap google.fr
+wget -O ~/.config/sublime-text-2/Packages/User/Preferences.sublime-settings https://github.com/Rainiugnas/ScriptFull/blob/master/sublime-settings.json
+wget -O ~/.config/sublime-text-2/Packages/User/Default\ \(Linux\).sublime-keymap https://raw.githubusercontent.com/Rainiugnas/ScriptFull/master/sublime-bind.json
